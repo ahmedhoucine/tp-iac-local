205542f08a5fe5b2132d4ebaf13d89330b5bf557
ahmedhoucine <ahmed.houcine@insat.ucar.tn>
2025-12-09 08:50:55 +0100

    init

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "db_name" {
+  description = "Nom de la base de données PostgreSQL"
+  type        = string
+  default     = "devops_db"
+}
+
+variable "db_user" {
+  description = "Nom d'utilisateur PostgreSQL"
+  type        = string
+  default     = "devops_user"
+}
+
+variable "db_password" {
+  description = "Mot de passe PostgreSQL"
+  type        = string
+  default     = "strongpassword123"
+}
+
+variable "app_port_external" {
+  description = "Port externe pour l'application web"
+  type        = number
+  default     = 8080
+}
